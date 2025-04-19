0a5dddeee35b3db0663357fd57c7b220cb9f7081
CarlosEReis <carlos.er7@gmail.com>
2025-04-19 13:05:06 -0300

    refactor: move endpoints de cartao

--- src/main/java/com/er7system/er7bank/api/controller/CartaoController.java
+++ src/main/java/com/er7system/er7bank/api/controller/CartaoController.java
@@ -3,40 +3,44 @@ package com.er7system.er7bank.api.controller;
 import com.er7system.er7bank.api.model.request.*;
 import com.er7system.er7bank.domain.dto.DadosPagamentoDTO;
 import com.er7system.er7bank.domain.model.Cartao;
+import com.er7system.er7bank.domain.model.TipoCartao;
+import com.er7system.er7bank.domain.service.CartaoCreditoService;
+import com.er7system.er7bank.domain.service.CartaoDebitoService;
 import com.er7system.er7bank.domain.service.CartaoService;
 import com.er7system.er7bank.domain.service.PagamentoService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+
 @RestController
 @RequestMapping("/v1/cartoes")
 public class CartaoController {
 
-    /*
-    - **PUT /cartoes/{id}/status** - Ativar ou desativar um cartão
-    - **PUT /cartoes/{id}/senha** - Alterar senha do cartão
-    - **GET /cartoes/{id}** - Obter detalhes de um cartão */
-
     private final CartaoService cartaoService;
     private final PagamentoService pagamentoService;
+    private final CartaoDebitoService cartaoDebitoService;
+    private final CartaoCreditoService cartaoCreditoService;
 
-    public CartaoController(CartaoService cartaoService, PagamentoService pagamentoService) {
+    public CartaoController(CartaoService cartaoService, PagamentoService pagamentoService, CartaoDebitoService cartaoDebitoService, CartaoCreditoService cartaoCreditoService) {
         this.cartaoService = cartaoService;
         this.pagamentoService = pagamentoService;
+        this.cartaoDebitoService = cartaoDebitoService;
+        this.cartaoCreditoService = cartaoCreditoService;
     }
 
-    //- **PUT /cartoes/{id}/status** - Ativar ou desativar um cartão
-    @PutMapping("/{idCartao}/status")
-    public ResponseEntity<Void> status(@PathVariable Integer idCartao, @RequestBody CartaoStatusRequest cartaoStatusRequest) {
-        cartaoService.alterarStatus(idCartao, cartaoStatusRequest.status());
-        return ResponseEntity.noContent().build();
-    }
+    @PostMapping
+    public ResponseEntity<Cartao> emitir(@RequestBody CartaoRequest cartaoRequest) {
+        // TODO: validar se credito ou debito
+        Cartao cartao = null;
+        if (TipoCartao.CREDITO.equals(cartaoRequest.tipo()))
+            cartao = cartaoCreditoService.criarCartao(cartaoRequest.conta(), cartaoRequest.diaVencimento());
 
-    //- **PUT /cartoes/{id}/senha** - Alterar senha do cartão
-    @PutMapping("/{idCartao}/senha")
-    public ResponseEntity<Void> senha(@PathVariable Integer idCartao, @RequestBody AtualizacaoSenhaRequest senhaRequest) {
-        cartaoService.alterarSenha(idCartao, senhaRequest.senhaAtual(), senhaRequest.novaSenha());
-        return ResponseEntity.noContent().build();
+        if (TipoCartao.DEBITO.equals(cartaoRequest.tipo()))
+            cartao = cartaoDebitoService.criarCartao(cartaoRequest.conta());
+
+        var uri = "http://localhost:8080/v1/cartoes/" + cartao.getId();
+        return ResponseEntity.created(URI.create(uri)).body(cartao);
     }
 
     @GetMapping("/{idCartao}")
@@ -54,6 +58,20 @@ public class CartaoController {
                 pagamentoRequest.valor(),
                 pagamentoRequest.tipoPagamento(),
                 pagamentoRequest.senha()));
+        return ResponseEntity.noContent().build();
+    }
+
+    //- **PUT /cartoes/{id}/status** - Ativar ou desativar um cartão
+    @PutMapping("/{idCartao}/status")
+    public ResponseEntity<Void> status(@PathVariable Integer idCartao, @RequestBody CartaoStatusRequest cartaoStatusRequest) {
+        cartaoService.alterarStatus(idCartao, cartaoStatusRequest.status());
+        return ResponseEntity.noContent().build();
+    }
+
+    //- **PUT /cartoes/{id}/senha** - Alterar senha do cartão
+    @PutMapping("/{idCartao}/senha")
+    public ResponseEntity<Void> senha(@PathVariable Integer idCartao, @RequestBody AtualizacaoSenhaRequest senhaRequest) {
+        cartaoService.alterarSenha(idCartao, senhaRequest.senhaAtual(), senhaRequest.novaSenha());
         return ResponseEntity.noContent().build();
     }
 
--- src/main/java/com/er7system/er7bank/api/controller/CartaoCreditoController.java
+++ src/main/java/com/er7system/er7bank/api/controller/CartaoCreditoController.java
@@ -1,51 +1,19 @@
 package com.er7system.er7bank.api.controller;
 
 import com.er7system.er7bank.api.model.request.*;
-import com.er7system.er7bank.domain.model.Cartao;
 import com.er7system.er7bank.domain.model.Fatura;
-import com.er7system.er7bank.domain.model.TipoCartao;
-import com.er7system.er7bank.domain.repository.CartaoRepository;
 import com.er7system.er7bank.domain.service.CartaoCreditoService;
-import com.er7system.er7bank.domain.service.CartaoDebitoService;
-import com.er7system.er7bank.domain.service.PagamentoService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.net.URI;
 
 @RestController
 @RequestMapping("/v1/cartoes")
 public class CartaoCreditoController {
-    private final CartaoDebitoService cartaoDebitoService;
 
+    private final CartaoCreditoService cartaoCreditoService;
 
-    /*
-        - **POST /cartoes** - Emitir um novo cartão
-        - **POST /cartoes/{id}/pagamento** - Realizar um pagamento com o cartão
-        - **PUT /cartoes/{id}/limite** - Alterar limite do cartão
-        - **GET /cartoes/{id}/fatura** - Consultar fatura do cartão de crédito
-        - **POST /cartoes/{id}/fatura/pagamento** - Realizar pagamento da fatura do cartão de crédito
-    */
-
-    private CartaoCreditoService cartaoCreditoService;
-
-    public CartaoCreditoController(CartaoCreditoService cartaoCreditoService, PagamentoService pagamentoService, CartaoRepository cartaoRepository, CartaoDebitoService cartaoDebitoService) {
+    public CartaoCreditoController(CartaoCreditoService cartaoCreditoService) {
         this.cartaoCreditoService = cartaoCreditoService;
-        this.cartaoDebitoService = cartaoDebitoService;
-    }
-
-    @PostMapping
-    public ResponseEntity<Cartao> emitir(@RequestBody CartaoRequest cartaoRequest) {
-        // TODO: validar se credito ou debito
-        Cartao cartao = null;
-        if (TipoCartao.CREDITO.equals(cartaoRequest.tipo()))
-            cartao = cartaoCreditoService.criarCartao(cartaoRequest.conta(), cartaoRequest.diaVencimento());
-
-        if (TipoCartao.DEBITO.equals(cartaoRequest.tipo()))
-            cartao = cartaoDebitoService.criarCartao(cartaoRequest.conta());
-
-        var uri = "http://localhost:8080/v1/cartoes/" + cartao.getId();
-        return ResponseEntity.created(URI.create(uri)).body(cartao);
     }
 
     @PutMapping("/{idCartao}/limite")
@@ -56,7 +24,7 @@ public class CartaoCreditoController {
     }
 
     @GetMapping("/{idCartao}/fatura/{idFatura}")
-    public ResponseEntity<Fatura> faturas(@PathVariable Integer idCartao, @PathVariable Integer idFatura) {
+    public ResponseEntity<Fatura> fatura(@PathVariable Integer idCartao, @PathVariable Integer idFatura) {
         var fatura = cartaoCreditoService.buscarFatura(idCartao, idFatura);
         return ResponseEntity.ok(fatura);
     }
